820b47ceac7dcf735d6afbf43efe03660bb4a6db
Tolga Tüfekçi <tolgatufekci@Tolga-MacBook-Pro.local>
2022-06-23 15:13:29 +0300

    navbar

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 const Color kLightBackground = Color(0xffeae8e8);
 
+const Color kDarkBlue = Color(0xff000099);
+
 const kSubTitleStyle = TextStyle(
     color: Colors.white,
     fontWeight: FontWeight.w700,
---
+++ lib/views/home_view.dart
@@ -0,0 +1,66 @@
+import 'package:calorie_diary/constants.dart';
+import 'package:calorie_diary/views/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+  final List<Widget> _views = [
+    const HomeScreen(),
+    Container(),
+    Container(),
+    Container(),
+    Container()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: _views.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: kDarkBlue,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 14,
+        iconSize: 24,
+        backgroundColor: kLightBackground,
+        showUnselectedLabels: true,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          if (index != 2) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          } else {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                useRootNavigator: true,
+                context: context,
+                builder: (context) {
+                  return Container();
+                });
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.calendar), label: 'Diary'),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.circlePlus,color: kDarkBlue,size: 42,), label: ''),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.chartLine), label: 'Progress'),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.user), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
